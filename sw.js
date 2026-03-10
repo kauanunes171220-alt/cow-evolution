cec6714f3901b4cc90880e34ae75a2d8567e62c6
MYKING <kauanunes.17.12.20@gmail.com>
2026-03-10 12:43:41 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'cow-evo-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responde com o cache quando estiver offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
